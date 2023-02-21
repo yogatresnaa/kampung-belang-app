f395bf010efb4585f26a2172f4ed7dfb16d97903
yogatresnaa <yogatresnaa@gmail.com>
2023-02-21 11:33:08 +0700

    add config url

--- src/scripts/globals/Config.js
+++ src/scripts/globals/Config.js
@@ -1,5 +1,9 @@
 const CONFIG = {
-  BASE_URL: 'http://localhost:5000/',
+  // Local
+  // BASE_URL: 'http://localhost:5000/',
+
+  // AWS EC2
+  BASE_URL: 'http://18.142.182.11:5000/',
 };
 
 export default CONFIG;
