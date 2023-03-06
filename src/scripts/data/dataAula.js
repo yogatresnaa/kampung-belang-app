dfe799d898d1c4161848e333a314b83cf99a7459
yogatresnaa <yogatresnaa@gmail.com>
2023-03-06 13:38:41 +0700

    update data aula

--- src/scripts/data/dataAula.js
+++ src/scripts/data/dataAula.js
@@ -3,68 +3,68 @@
 let aula = [
   {
     id: 1,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'View 1',
+    description: 'Cocok bagi anda yang ingin mengadakan acara dengan konsep di luar ruangan, dengan suasana teduh dibawah pohon sehingga tidak terlalu panas ketika disiang hari',
     imageMenu: './asset/aula1.JPG',
   },
 
   {
     id: 2,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'View 2',
+    description: 'Pas untuk anda yang ingin mengadakan rapat organisasi dan pertemuan lainnya',
     imageMenu: './asset/aula2.JPG',
   },
 
   {
     id: 3,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'View 3',
+    description: 'Kami juga menyediakan ruangan dan perlengkapan bagi anda yang ingin mengadakan acara dalam ruangan seperti acara ulang tahun dll',
     imageMenu: './asset/aula3.JPG',
   },
 
   {
     id: 4,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'view 4',
+    description: 'Bagi anda yang ingin mengadakan seminar ataupun workshop, kami juga menyediakan tempat dan peralatan untuk menunjang kegiatan tersebut',
     imageMenu: './asset/aula4.JPG',
   },
 
   {
     id: 5,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'view 5',
+    description: 'Kami juga meyediakan ruangan bagai anda yang ingin bersantai dengan keluarga atau pasangan anda ',
     imageMenu: './asset/aula5.JPG',
   },
 
   {
     id: 6,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'view 6',
+    description: 'Ruangan ini bisa menampung banyak orang, jadi anda tidak perlu merasa khawatir ',
     imageMenu: './asset/aula6.JPG',
   },
 
   {
     id: 7,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'view 7',
+    description: 'Bagi anda yang ingin menghilangkan rasa penat setelah pulang kerja atau kuliah, suasanan malam dengan lampu yang indah bisa membuat anda santai',
     imageMenu: './asset/aula7.JPG',
   },
   {
     id: 8,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'view 8',
+    description: 'Selain dari pada itu, kami juga memiliki rooftop dengan pemandangan yang indah. Bagi anda yang suka suasana di atas rooftop ini cocok banget untuk kamu santai bareng teman-teman',
     imageMenu: './asset/aula8.JPG',
   },
   {
     id: 9,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'view 9',
+    description: 'Kebersihan adalah menjadi ciri utama tempat kami, selain ruangan yang nyaman, kebersihan menjadi nilai utama di tempat kami',
     imageMenu: './asset/aula9.JPG',
   },
   {
     id: 10,
-    title: 'Lorem ipsum dolor sit amet',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas qui minima iusto amet magni',
+    title: 'view 10',
+    description: 'Bagi teman-teman yang muslim, kami juga menyediakan musolla dengan Ac. agar semua tamu kami yang muslim bisa nyaman dalam beribadah',
     imageMenu: './asset/aula10.JPG',
   },
 ];
